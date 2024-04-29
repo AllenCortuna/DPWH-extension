7a1a832afc34d83985f013dea65d22a599752858
zanzen <olsencortuna@gmail.com>
2024-04-29 14:49:06 +0800

    patch: if condition includes

--- content.js
+++ content.js
@@ -39,7 +39,7 @@ const data = {
 const currentURL = window.location.href;
 
 // CIVIL WORKS
-if (currentURL.includes("https://www.dpwh.gov.ph/DPWH/business/procurement/civil_works/form")) {
+if (currentURL.includes("civil_works/form") || currentURL.includes("https://www.dpwh.gov.ph/dpwh/business/procurement/civil_works/form")) {
   const categoryInput = document.querySelector("#edit-submitted-category");
   if (categoryInput) {
     categoryInput.value = data.category;
@@ -74,7 +74,7 @@ if (currentURL.includes("https://www.dpwh.gov.ph/DPWH/business/procurement/civil
 }
 
 // GOODS
-if (currentURL.includes("https://www.dpwh.gov.ph/DPWH/business/procurement/goods_services/form")) {
+if (currentURL.includes("goods_services/form") || currentURL.includes("https://www.dpwh.gov.ph/dpwh/business/procurement/goods_services/form")) {
   const categoryInput = document.querySelector("#edit-submitted-category");
   if (categoryInput) {
     categoryInput.value = data.goodsCategory;
